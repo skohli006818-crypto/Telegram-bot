485126e9350ef50e2093a0f04e424a2ace2481b5
skohli006818-crypto <skohli006818@gmail.com>
2026-03-24 23:07:19 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -133,4 +133,7 @@ async def main():
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    import asyncio
+    loop = asyncio.get_event_loop()
+    loop.create_task(main())
+    loop.run_forever()
